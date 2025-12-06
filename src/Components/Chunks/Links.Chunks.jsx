0ed0f6703bb892a25e01cb795511586f0dff3479
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-12-06 12:26:45 +0530

    Links for courses updated

---
+++ src/Components/Chunks/Links.Chunks.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import axios from 'axios';
+
+function Links_Chunks() {
+     const API = 'https://server-01-v2cx.onrender.com/getcourse';
+     const [data, setData] = useState(null);
+
+     useEffect(() => {
+          const fetchData = async () => {
+               const responce = await axios.get(API);
+               setData(responce.data);
+          }
+          fetchData();
+     }, []);
+
+  return (
+    <div className='w-full flex flex-col gap-4 items-center h-20 py-3 font-color'>
+     <h1 className='text-3xl font-medium w-full text-center'>Quick link</h1>
+     <p className='w-full text-center opacity-60 font-light'>Easily access important pages, resources, and services, and services all in one place</p>
+     <div className='w-full max-w-[1200px] grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3'>
+          {data ? data.map((items, index) => (
+               <NavLink to={`/course/${items._id}`} key={index} className='w-full h-40 bg-zinc-800 rounded-2xl p-3 flex flex-col justify-between items-start gap-2 hover:bg-[#393E46]/40 transition-all border border-zinc-700/30'>
+                    <div>
+                         <h1 className='text-xl font-medium poppins'><i className={`${items.icon} text-2xl font-light`}></i>{items.title}</h1>
+                         <p className='text-sm font-extralight opacity-70 poppins line-clamp-2'>{items.description}</p>
+                    </div>
+                    <div>
+                         <NavLink to={`/course/${items._id}`} className='text-sm font-mono underline underline-offset-2 hover:text-blue-500 opacity-70 poppins'>Explore Now <i className='ri-arrow-right-line'></i></NavLink>
+                    </div>
+               </NavLink>
+          )) : <div className='text-lg font-light opacity-60'>Loading...</div>}
+     </div>
+    </div>
+  )
+}
+
+export default Links_Chunks
--- src/Pages/Landing.Page.jsx
+++ src/Pages/Landing.Page.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import Nav_Chunk from '../Components/Chunks/Nav.Chunk'
 import Header_Chunk from '../Components/Chunks/Header.Chunk'
 import Hero_Chunk from '../Components/Chunks/Hero.Chunk'
+import Links_Chunks from '../Components/Chunks/Links.Chunks';
 
 function Landing_Page() {
-     const [head, setHead] = useState(false)
+     const [head] = useState(false);
   return (
     <div className='w-full min-h-screen flex flex-col items-center justify-start gap-2 p-2'>
      <div className={`max-h-12 w-full bg-green-400`}>
@@ -12,6 +13,7 @@ function Landing_Page() {
      </div>
      <Nav_Chunk/>
      <Hero_Chunk/>
+     <Links_Chunks/>
     </div>
   )
 }
